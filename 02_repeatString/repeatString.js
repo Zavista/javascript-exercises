f8f8ef9dc93b70ef84ce149b04fb2f8f51365932
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-07 17:20:58 -0400

    Completed Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let ans = "";
+    for (let i = 0; i < n; i++){
+        ans = ans + str; //appends str to ans
+    }
+    
+    if (n < 0){
+        return "ERROR";
+    }
+
+    return ans;
 
 };
 
