f586eb9105b65a09fd865550fad406d5b36ac583
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-07 19:24:57 -0400

    Completed excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let sum = 0;
+    if (start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number' ){
+        //Checks for error cases
+        //when start or end is negative
+        //when start or end is NaN (not a number)
+        return 'ERROR';
+    }
+    else if (start < end){
+      for (let i = start; i < end + 1; i++){ //stop at end+1 b/c we want to include end 
+        sum = sum + i;
+        }  
+    }
+    else{
+        for (let i = end; i < start + 1; i++){
+            sum = sum + i;
+        }
+    }
+    return sum;
 
 };
 
