922e2c242c323fb8b0777a565ab86f0b89541e3d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-07 17:29:18 -0400

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let listofChar = str.split(""); //splits the str at "" so each char becomes a member of the array
+    listofChar = listofChar.reverse()
+    return listofChar.join(""); //joins each element back into a string
 };
 
 // Do not edit below this line
