21e4a523a241baf162ec4d9260ccb4417cd6edad
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-10 15:46:30 -0400

    Completed palidrome exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+
+    function isLetter(char){
+        return char.match(/[a-z0-9]/i); //checks if char is in the character class form a-z and the i signifies capitalizaiton doesn't matter
+        //OR if char is in 0-9
+    }
+
+
+    let strList = str.split(""); //turns str into a list of characters
+    strList = strList.filter(isLetter).map(char => char.toLowerCase());
+    //filters stringList to only alphabet characters. 
+    //map to convert each character into lowercase
+
+
+
+    let left = 0;
+    let right = strList.length - 1;
+
+    while (left < right){
+        if (strList[left] != strList[right]){
+            return false;
+        }
+        left++;
+        right--;
+    } 
+    return true;
+
+    //could also return strList.reverse().join("") == str;
 
 };
 
