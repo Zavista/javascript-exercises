27065d9d3003793123f03fea30cee59f6620fced
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-10 16:01:06 -0400

    Completed exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+
+    if (n < 0){
+        return "OOPS";
+    }
+    else if (n === 0){
+        return 0;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+    //we will start at 2nd element of [1, 1, 2, 3, 5] which has firstPrev =1 and secondPrev = 0;
+    //will stop right before nth element
+    for (let i = 2; i < n; i++){
+        const curr = secondPrev + firstPrev
+        secondPrev = firstPrev;
+        firstPrev = curr;
+    }
+
+    return secondPrev + firstPrev;
+
 
 };
 
