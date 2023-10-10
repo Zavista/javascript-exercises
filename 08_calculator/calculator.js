414bcf98a11f7316a9989c17f86f56058d44baff
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-10 15:22:31 -0400

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {
+    total = total + num;
+    return total;
+  }, 0)
+
+  //using reduce to iterate through each element in the array and add it to total, start with a total of 0.
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {
+    total = total*num;
+    return total;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let total = 1; //cause 0! is still 1, its our base case
+	for (let i = n; i > 0; i--){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
