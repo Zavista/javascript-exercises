6e4978ab1e3d54448bc9ad16affba73afe65a0a0
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-10 16:34:55 -0400

    Completed exercsie 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    
+   const getAge = function(birth, death){
+        if (!death){ //when yearofDeath does not exit, we get undefined which is false so !(undefined) = true. when given a num,  !(death) is false.
+            return new Date().getFullYear() - birth;
+        }
+        else{
+            return death - birth;
+        }
+    }
 
+    return peopleArr.reduce((oldest, person) =>{
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+
+        if (personAge > oldestAge){
+            oldest = person
+        }//since we dont have an initial after {funciton body}, .reduce() will have first element be initial and start from the second
+
+        return oldest; //return oldest for the next iteration
+    }); 
+    
 };
 
 // Do not edit below this line
